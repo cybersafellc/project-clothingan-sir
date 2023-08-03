2b69ae2078aefb5f23e59eb1696d5e29ac57def7
unknown <rusnandapurnama@gmail.com>
2023-08-03 10:59:52 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// operator matematika
+// operator aritmatika
+console.log(1 + 4);
+console.log(5 - 1);
+console.log(5 / 1);
+console.log(5 * 1);
+
+// operator augmented assigment
+let nilaiAwal = 10;
+
+nilaiAwal += 1;
+console.log(nilaiAwal);
+
+nilaiAwal -= 1;
+console.log(nilaiAwal);
+
+nilaiAwal /= 5;
+console.log(nilaiAwal);
+
+nilaiAwal *= 5;
+console.log(nilaiAwal);
+
+// operator unary
+nilaiAwal++;
+console.log(nilaiAwal);
+
+nilaiAwal--;
+console.log(nilaiAwal);
+
+// operator perbandingan
+// operator perbandingan adalah pembandingan antara 2 nilai yang hasilnya adalah nilai boolean (true/false)
+
+let nilaiPerbandingan = 1 < 2;
+console.log(nilaiPerbandingan);
+nilaiPerbandingan = 1 > 2;
+console.log(nilaiPerbandingan);
+
+// operator logika
+// operator yang membandingkan 2 buah nilai boolean yang hasilnya nanti nilai boolean lagi
+console.log(true && false);
+console.log(true || false);
+console.log(5 < 10 && 10 < 5);
+console.log(5 < 10 || 10 < 5);
+
+let nilaiKebalikan = true;
+console.log(!nilaiKebalikan);
